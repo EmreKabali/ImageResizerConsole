5430ef073e3c30f90a92aaf8fa92d23881921e3f
EmreKabali <kabali68@gmail.com>
2024-05-18 23:21:57 +0300

    first commit

---
+++ ConsoleApp1/ImageHelper.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp1;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System.IO;
+public static class ImageHelper
+{
+
+    public static byte[] CompressImage(byte[] imageBytes, int quality, int width, int height)
+    {
+
+        using (var image = Image.Load<Rgba32>(imageBytes))
+        {
+            // Calculate the new size while maintaining the aspect ratio
+            Size newSize = CalculateNewSize(image.Width, image.Height, width, height);
+
+            // Resize the image
+            image.Mutate(x => x.Resize(new ResizeOptions
+            {
+                Size = newSize,
+                Mode = ResizeMode.Max
+            }));
+
+            var jpegOptions = new JpegEncoder
+            {
+                Quality = quality // Set the quality level
+            };
+
+            using (var outputStream = new MemoryStream())
+            {
+                image.Save(outputStream, jpegOptions);
+                return outputStream.ToArray();
+            }
+        }
+    }
+
+    private static Size CalculateNewSize(int originalWidth, int originalHeight, int maxWidth, int maxHeight)
+    {
+        double widthRatio = (double)maxWidth / originalWidth;
+        double heightRatio = (double)maxHeight / originalHeight;
+        double ratio = Math.Min(widthRatio, heightRatio);
+
+        int newWidth = (int)(originalWidth * ratio);
+        int newHeight = (int)(originalHeight * ratio);
+
+        return new Size(newWidth, newHeight);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+string imagePath = "adsiztasarim.png"; // Küçültülecek resmin dosya yolu
+string outputPath = Guid.NewGuid().ToString() + "-img.jpg"; // Küçültülmüş resmin kaydedileceği dosya yolu
+int quality = 20; // Kalite seviyesi (1-100 arası)
+int newWidth = 800; // Yeni genişlik
+int newHeight = 600;
+
+byte[] imageBytes = File.ReadAllBytes(imagePath);
+byte[] compressedImageBytes = ImageHelper.CompressImage(imageBytes, quality, newWidth, newHeight);
+
+File.WriteAllBytes(outputPath, compressedImageBytes);
+
